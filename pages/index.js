3d818eafd8dc97a5450ba5ff1a5f8cd8da12efff
wylited <wylited@gmail.com>
2022-10-02 21:34:11 +0800

    fix build error and add sidebar and navbar

--- pages/index.js
+++ pages/index.js
@@ -7,22 +7,38 @@ export default function Home() {
 
     // Hero Titlepage
     <div className={styles.container}>
-      <Head>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-          <img src="https://github.com/wylited/KGVMedicalSociety/raw/main/public/KGVMedSociety.png" class="max-w-sm rounded-lg shadow-2xl" />
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello There!</h1>
-              <p className="py-6">Get updated on the latest medical discoveries!</p>
-              <button className="btn btn-primary">Find out more.</button>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+
+          <div className="navbar bg-base-100">
+            <div className="navbar-start">
+              <label htmlFor="my-drawer" className="btn btn-ghost drawer-button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
+              <a className="btn btn-ghost normal-case text-xl">KGV Medical Society</a>
             </div>
           </div>
-        </div>
-      </Head>
 
-      {/* main content goes here, including posts */}
-      <main className={styles.main}>
-      </main>
+          <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+            <img src="https://github.com/wylited/KGVMedicalSociety/raw/main/public/KGVMedSociety.png" class="max-w-sm rounded-lg shadow-2xl" />
+              <div className="max-w-md">
+                <h1 className="text-5xl font-bold">Hello There!</h1>
+                <p className="py-6">Get updated on the latest medical discoveries!</p>
+                <button className="btn btn-primary">Find out more.</button>
+              </div>
+            </div>
+          </div>
+        </div> 
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+            
+          </ul>
+        </div>
+      </div>
 
       {/* Footer */}
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
